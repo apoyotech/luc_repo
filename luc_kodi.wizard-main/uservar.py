1b3bcf4a852fd9dac1f6de117182b023597d2b6c
apoyotech <luciangazdaru@gmail.com>
2024-10-03 16:10:33 +0200

    Add files via upload

---
+++ luc_kodi.wizard-main/uservar.py
@@ -0,0 +1,8 @@
+'''#####-----Build File-----#####'''
+buildfile = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'http://CHANGEME'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
